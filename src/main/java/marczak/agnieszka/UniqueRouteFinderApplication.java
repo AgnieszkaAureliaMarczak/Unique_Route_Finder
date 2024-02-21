b4bc40adbaf3632357990d1fd2a5bba0cc7b185e
AgnieszkaMarczak <agnieszka.aurelia.marczak@gmail.com>
2024-02-21 20:35:56 +0100

    Initial commit

---
+++ src/main/java/marczak/agnieszka/UniqueRouteFinderApplication.java
@@ -0,0 +1,12 @@
+package marczak.agnieszka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniqueRouteFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniqueRouteFinderApplication.class, args);
+	}
+}
---
+++ src/test/java/marczak/agnieszka/UniqueRouteFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package marczak.agnieszka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueRouteFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
